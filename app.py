0154a9db1671b0265a1ac850ddf33a662c1952dd
Sonia <sonia.illanasc@me.com>
2022-03-16 21:33:09 +0100

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(
     page_title="iMammalia App", page_icon="📊" #
 )
 
-#main_container=st.container()
+
 
 st.title('iMammalia App')
 st.write('Comparing data collected from citizen science versus data available in the Global Biodiversity Information Facilities (GBIF)')
@@ -32,21 +32,18 @@ gbif_df = pd.read_csv(os.path.join('species', gbif_specie))
 
 estado_iucn = gbif_df['iucnRedListCategory'].unique()[0]
 
-st.markdown('[Information related with the species?](https://www.google.com/search?q='+specie.replace(" ", "+")+'+species)')
+st.markdown('[Information related with the species](https://www.google.com/search?q='+specie.replace(" ", "+")+'+species)')
 st.markdown('[Contribute with more observation of the species](https://mammalnet.net/es/imammalia)')
 st.markdown('[Share your camera traps photos](https://www.mammalweb.org/en/login)')
 
 
 with st.container():
-    st.write('Species information')
+    st.write('Species registers')
     col1, col2, col3 = st.columns(3)
     col1.metric('iMammalia registers', len(dm_specie))
     col2.metric('GBIF registers (in the world)', len(gbif_df))
     col3.metric('IUCN status', estado_iucn)
     
-
-
-
 
 gbif_coords = gbif_df[['lon', 'lat']].dropna()
 dm_specie = dm_specie[['lon', 'lat']].dropna()
@@ -66,6 +63,14 @@ dm_specie = dm_specie[['lon', 'lat']].dropna()
 #   gbif_coords["gbif_icon"][i] = gbif_icon
 
 
+st.subheader('Distribution of the '+specie)
+slider_range=st.slider('Select year for GBIF observed data', min_value=1900, max_value=2022, 
+                       value=[2018, 2022])
+year_min=slider_range[0]
+year_max=slider_range[1]
+
+st.write(year_min)
+st.write(year_max)
 
 st.pydeck_chart(pdk.Deck(
      map_style='mapbox://styles/mapbox/light-v9',
@@ -112,5 +117,4 @@ st.pydeck_chart(pdk.Deck(
      ]
  ))
 
-
-
+st.markdown('_GBIF data showed correspond to a dataset downloaded the 2022-03-10, which doi is_ '+'download1[1,2]'+' _and has been filtered to a coordinate precision equal or below to 2000m_.')
