e2284d96564c5da4ae88554a2404378780639ed3
Sonia <sonia.illanasc@me.com>
2022-03-21 13:28:07 +0100

    app final

--- app.py
+++ app.py
@@ -14,23 +14,22 @@ st.set_page_config(
 
 ###Define color theme
 ##libraries for define theme
-#import requests
-#from pathlib import Path
-#import random
-#import utils
-#
-#utils.local_css("local_styles.css")
-#
-## Init state. This is only run whenever a new session starts (i.e. each time a new
-## browser tab is opened).
-#if not st.session_state:
-#    st.session_state.primaryColor = "#27a5da"
-#    st.session_state.backgroundColor = "#f7fbfd"
-#    st.session_state.secondaryBackgroundColor = "#d6dcde"
-#    st.session_state.textColor = "#171b29"
-#    st.session_state.is_dark_theme = True
-#    st.session_state.first_time = True
+import requests
+from pathlib import Path
+import random
+import utils
 
+utils.local_css("local_styles.css")
+
+# Init state. This is only run whenever a new session starts (i.e. each time a new
+# browser tab is opened).
+if not st.session_state:
+    st.session_state.primaryColor = "#27a5da"
+    st.session_state.backgroundColor = "#f7fbfd"
+    st.session_state.secondaryBackgroundColor = "#d6dcde"
+    st.session_state.textColor = "#171b29"
+    st.session_state.is_dark_theme = True
+    st.session_state.first_time = True
 
 st.image('https://mammalnet.net/wp-content/uploads/2021/04/cropped-logo.png', width=400)
 st.write('Comparing data collected from citizen science _versus_ data available in the Global Biodiversity Information Facilities (','[GBIF](https://www.gbif.org/)', ').')
